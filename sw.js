f601ffa2d8c0733388ad376a6cb0faf41a277c4c
Sujai-01 <spanigra5@gitam.in>
2026-05-14 13:32:29 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='drrigved-v2';
-const ASSETS=['./index.html','./about.html','./research.html','./faq.html','./blog.html','./gallery.html','./shared.css'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
